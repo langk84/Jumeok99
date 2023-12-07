4ec9de1085d43572de26f16924695b5f74cce915
langk84 <langk@syuin.ac.kr>
2023-12-07 15:08:08 +0900

    clean code server.js

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ app.use(static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
 	console.log('요청받음');
 
-	var paramId = req.body.id || req.query.id;
-	var paramPassword = req.body.password || req.query.password;
+	var paramcode = req.body.code || req.query.code;
+	var paramtime = req.body.time || req.query.time;
 	
 	res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 	res.write('<h1>요청 수락됨</h1>');
-	res.write('<div><p>요청한 주식 : ' + paramId + '</p></div>');
-	res.write('<div><p>학습 횟수 : ' + paramPassword + '</p></div>');
-    res.write('<div><p>결과 페이지localhost/'+paramId+','+paramPassword+'.png</p></div>');
+	res.write('<div><p>요청한 주식 : ' + paramcode + '</p></div>');
+	res.write('<div><p>학습 횟수 : ' + paramtime + '</p></div>');
+    res.write('<div><p>결과 페이지 : localhost/'+paramcode+','+paramtime+'.png</p></div>');
 	res.end();
-    console.log(paramId, paramPassword);
+    console.log(paramcode, paramtime);
     const fs = require('fs');
-    fs.writeFile('require.txt', paramId+'\n'+paramPassword, (err) => {
+    fs.writeFile('require.txt', paramcode+'\n'+paramtime, (err) => {
         if (err) console.log('Error: ', err);
         else console.log('File created');
 });
--- test.py
+++
@@ -1,3 +0,0 @@
-for i in range(10):
-    if(i!=4 and i!=6):
-        print(i,end='')
